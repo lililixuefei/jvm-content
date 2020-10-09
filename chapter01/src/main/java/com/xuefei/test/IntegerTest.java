8a802c3142682169cbd9db81ad197844eb52683e
xuefei <13273726905@163.com>
2020-10-09 22:46:15 +0800

    初始化

---
+++ chapter01/src/main/java/com/xuefei/test/IntegerTest.java
@@ -0,0 +1,22 @@
+package com.xuefei.test;
+
+/**
+ * @description:
+ * @author: xuefei
+ * @date: 2020/10/09 22:38
+ */
+public class IntegerTest {
+    public static void main(String[] args) {
+        Integer x = 5;
+        int y = 5;
+        System.out.println(x == y);
+
+        Integer i1 = 10;
+        Integer i2 = 10;
+        System.out.println(i1 == i2);
+
+        Integer i3 = 128;
+        Integer i4 = 128;
+        System.out.println(i3 == i4);
+    }
+}
